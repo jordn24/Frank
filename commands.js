6da7434ee6e272e91a8032067ad9dcc2fb24c188
Jordan <jordan.sposito2430@gmail.com>
2022-08-09 21:48:29 +1000

    commands initial commit

--- commands.js
+++ commands.js
@@ -1,15 +1,32 @@
 const discord = require('discord.js')
+const fs = require('fs');
 
-const { token } = require('./config.json');
-const { leaderboard } = require('./data.json')
+const { token, channelid } = require('./config.json');
 
 const client = new discord.Client();
 
 client.on('message', async message => {
-    if (message.content.toLowerCase() == '!leaderboard'){
-        // Get data from data json
+    let data = fs.readFileSync('data.json');
+    var leaderboard = JSON.parse(data)
+    leaderboard = leaderboard["leaderboard"]
+
+    if (message.content.toLowerCase() == '!scoreboard'){
+        // Fields
+        let fields = []
+
+        for (var key of Object.keys(leaderboard)) {
+            fields.push({name: key + ":", value: "Has bottom fragged " + String(leaderboard[key]) + " time/s"})
+        }
+
         // Message embed
+        embed = new discord.MessageEmbed()
+                    .setColor(0xF21515)
+                    .setTitle("Leaderboard")
+                    .addFields(fields)
+                    .setTimestamp()
+                    .setFooter("Updated every 30 minutes")
         // Send message
+        client.channels.cache.get(channelid).send(embed)
     }
 })
 
